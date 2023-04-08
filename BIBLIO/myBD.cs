5f86f94947f8ef12c2fcf374609628cabd5eb3d8
ASUS <ASUS@DESKTOP-RGIE6CQ>
2023-04-08 23:57:55 +0300

    Add project files.

---
+++ BIBLIO/myBD.cs
@@ -0,0 +1,27 @@
+namespace BIBLIO
+{
+    public partial class myBD : Form
+    {
+        public myBD()
+        {
+            InitializeComponent();
+        }
+
+        private void âèõ³äToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ïğîĞîãğàìóToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            about f1 = new about();
+            f1.ShowDialog();
+        }
+
+        private void êàëüêóëÿòîğToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Calculator f2 = new Calculator();   
+            f2.ShowDialog();
+        }
+    }
+}
